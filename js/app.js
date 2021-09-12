f60c0fbdc6c8023597f9ad21d22f759f0105a733
Antardas <antardas2334@gmail.com>
2021-09-13 01:49:17 +0600

    Add Average Rating & Given Rating

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ loadProducts();
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
+
+    // Destrcturing Object
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -21,6 +23,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <h5>⭐<span>${product.rating.rate}</span>(${product.rating.count})</h5>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -82,3 +85,5 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
+
